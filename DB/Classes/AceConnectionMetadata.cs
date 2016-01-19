3031319e9fdf7d5b48c55470d458acd0db28c9e9
jaerith <Aaron.Kendall@gmail.com>
2016-01-19 10:59:09 -0500

    * Added connection class.

---
+++ DB/Classes/AceConnectionMetadata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.DB.Classes
+{
+    /// <summary>
+    /// 
+    /// This class serves as the configuration data needed in order to establish
+    /// a connection with the database.
+    ///     
+    /// </summary>
+    public class AceConnectionMetadata
+    {
+        public AceConnectionMetadata()
+        {
+
+            DBUser     = "";
+            DBPassword = "";
+            DBTarget   = "";
+            DBMachine  = "";
+
+            DBConnectionString = "";
+        }
+
+        public string DBUser { get; set; }
+
+        public string DBPassword { get; set; }
+
+        public string DBTarget { get; set; }
+
+        public string DBMachine { get; set; }
+
+        public string DBConnectionString { get; set; }
+    }
+}
+
