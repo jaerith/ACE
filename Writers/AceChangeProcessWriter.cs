f476eab91f37068da0dc000fc4e37164789a7db4
jaerith <Aaron.Kendall@gmail.com>
2016-02-12 12:25:17 -0500

    * Began to write a few lines for the ProcessWriter.

--- Writers/AceChangeProcessWriter.cs
+++ Writers/AceChangeProcessWriter.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ACE.DB.Classes;
+
 namespace ACE.Writers
 {
-    class AceChangeProcessWriter
+    /// <summary>
+    /// 
+    /// This class serves to record an instance of a particular job running against its 
+    /// configured REST API.
+    ///     
+    /// </summary>
+    public class AceChangeProcessWriter : IDisposable 
     {
+        public AceChangeProcessWriter()
+        { }
+
+        public void Dispose()
+        { }
     }
 }
