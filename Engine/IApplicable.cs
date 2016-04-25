809dbf7dd2a4ab36abfb46755a022325550b1835
jaerith <Aaron.Kendall@gmail.com>
2016-04-25 16:58:45 -0400

    * Created the IApplicable interface for classes that upsert records parsed from raw data payloads.

---
+++ Engine/IApplicable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Engine
+{
+    /// <summary>
+    /// 
+    /// This interface should be inherited by a class that will correctly implement the method(s)
+    /// declared here.  Namely, when upserting a record, the implementating class should only 
+    /// mark a database record as updated when there is an actual difference detected between
+    /// the incoming record and the present record.
+    ///     
+    /// </summary>
+    public interface IApplicable
+    {
+        bool InitPreparedStatements();
+
+        bool CompareOldVersusNew(Hashtable poOldRecord, Hashtable poNewRecord);
+
+        bool UpsertRecord(Hashtable poRecord);
+    }
+}
