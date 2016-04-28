cd2519c371fcdc985c605b89311f6d228e8b239a
jaerith <Aaron.Kendall@gmail.com>
2016-04-28 16:08:46 -0400

    * Created the AceApplyManager class.

---
+++ Writers/AceApplyManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Writers
+{
+    /// <summary>
+    /// 
+    /// This class will inherit the IApplicable interface and serve as our default class for 
+    /// upserting records (parsed from the raw payload) into target columns on the database.
+    ///     
+    public class AceApplyManager
+    {
+        public AceApplyManager()
+        { }
+    }
+}
