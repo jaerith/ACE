3d15583ee4675087978453684623c7daf9803df1
Aaron Kendall <AKendall@bn.com>
2016-05-02 15:39:22 -0400

    * Started the implementation of the default ApplyManager class.

--- Writers/AceApplyManager.cs
+++ Writers/AceApplyManager.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using ACE.DB.Classes;
 
 namespace ACE.Writers
 {
@@ -11,9 +15,59 @@ namespace ACE.Writers
     /// This class will inherit the IApplicable interface and serve as our default class for 
     /// upserting records (parsed from the raw payload) into target columns on the database.
     ///     
-    public class AceApplyManager
+    public class AceApplyManager : IDisposable, IApplicable
     {
-        public AceApplyManager()
-        { }
+        private SqlConnection DbConn;
+
+        private SqlCommand CountStatement;
+        private SqlCommand InsertStatement;
+        private SqlCommand UpdateStatement;
+        private SqlCommand RetrieveStatement;
+
+        private AceAPIBucket BucketConfiguration;
+
+        public AceApplyManager(AceAPIBucket poBucketConfiguration)
+        {
+            DbConn = null;
+
+            CountStatement = InsertStatement = UpdateStatement = CountStatement = null;
+
+            BucketConfiguration = poBucketConfiguration;
+
+            InitPreparedStatements(poBucketConfiguration);
+        }
+
+        #region IDisposable() method
+        public void Dispose()
+        {
+            if (DbConn != null)
+            {
+                DbConn.Close();
+                DbConn = null;
+            }
+        }
+        #endregion
+
+        #region IApplicable methods
+        public bool InitPreparedStatements(AceAPIBucket poBucketConfiguration)
+        {
+            // Finish implementation
+            return true;
+        }
+
+
+        public bool CompareOldVersusNew(Hashtable poOldRecord, Hashtable poNewRecord)
+        {
+            // Finish implementation
+            return true;
+        }
+
+
+        public bool UpsertRecord(Hashtable poRecord)
+        {
+            // Finish implementation
+            return true;
+        }
+        #endregion
     }
 }
