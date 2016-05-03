63b0af844984649b934c392b38ddd6b47b388e2b
jaerith <Aaron.Kendall@gmail.com>
2016-05-03 17:09:58 -0400

    * Started to implement the InitPreparedStatements method.

--- Writers/AceApplyManager.cs
+++ Writers/AceApplyManager.cs
@@ -49,8 +49,29 @@ namespace ACE.Writers
         #endregion
 
         #region IApplicable methods
+        /// <summary>
+        /// 
+        /// This method will intialize the prepared statements that will be constructed via direction 
+        /// from the provided metadata.  These statements will then be used to 
+        /// 
+        /// <param name="poBucketConfiguration">The data from the configured Process</param>
+        /// <returns>The ID representing the instance of the Process that failed previously</returns>
         public bool InitPreparedStatements(AceAPIBucket poBucketConfiguration)
         {
+            StringBuilder CountStatement  = new StringBuilder("SELECT COUNT(*) FROM " + poBucketConfiguration.TableName);
+            StringBuilder SelectStatement = new StringBuilder("SELECT ");
+            StringBuilder InsertStatement = new StringBuilder("INSERT INTO " + poBucketConfiguration.TableName + "(");
+            StringBuilder UpdateStatement = new StringBuilder("UPDATE " + poBucketConfiguration.TableName + " SET ");
+
+            foreach (string sTmpColumn in poBucketConfiguration.ColKeys)
+            {
+                if (SelectStatement.Length > 0)
+                    SelectStatement.Append(", ");
+
+                SelectStatement.Append(sTmpColumn);
+                InsertStatement.Append(sTmpColumn);
+            }
+
             // Finish implementation
             return true;
         }
