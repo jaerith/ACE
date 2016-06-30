f98315788abb728beecfa60088fc6b837a18e649
jaerith <Aaron.Kendall@gmail.com>
2016-06-30 12:17:09 -0400

    * Added column for request header arguments.

--- Sql/Tables/ace_cfg_api.sql
+++ Sql/Tables/ace_cfg_api.sql
@@ -16,6 +16,7 @@ CREATE TABLE [dbo].[ACE_CFG_API](
  [PULLS_SINGLE_ITEM_FLAG] [char](1)              NOT NULL,
  [TARGET_KEY_LIST]        [varchar](4000)        NOT NULL,
  [CONTENT_TYPE]           [varchar](8)           NOT NULL,
+ [REQUEST_HDR_ARGS]       [varchar](2000)        NOT NULL,
  [ADD_DTIME]              [datetime]             NOT NULL,
  [UPD_DTIME]              [datetime]             NOT NULL,
 CONSTRAINT [PK_ACE_CFG_API] PRIMARY KEY CLUSTERED 
